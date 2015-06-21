a67df38709a88b44bcb47fb755e4a7d607d81bcf
Alexandre Bodelot <alexandre.bodelot@gmail.com>
2015-06-21 12:48:57 +0200

    Added Terminator configuration

--- deploy.sh
+++ deploy.sh
@@ -14,3 +14,8 @@ git clone --depth=1 https://github.com/bling/vim-airline.git
 git clone --depth=1 https://github.com/airblade/vim-gitgutter.git
 git clone --depth=1 https://github.com/tpope/vim-surround.git
 
+# Terminator
+# ------------------------------------------------------------------------------
+
+cp --parents terminator/config ~/.config
+
