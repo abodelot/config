7a8a8c466f8256397a25b08b3e3d711722fe5ed8
Alexandre Bodelot <alexandre.bodelot@gmail.com>
2015-10-10 15:06:14 +0200

    Use functions in deploy script, re-enabled terminator

--- deploy.sh
+++ deploy.sh
@@ -4,65 +4,86 @@ C_GREEN="\033[1;32m"
 C_YELLOW="\033[1;33m"
 C_RESET="\033[0m"
 
-# Bash
+# Deploy functions
 # ------------------------------------------------------------------------------
 
-echo -e "${C_GREEN}* bash${C_RESET}"
-cp .bashrc ~/.bashrc -v
+DeployBash() {
+    echo -e "${C_GREEN}* bash${C_RESET}"
+    cp .bashrc ~/.bashrc -v
+}
 
-# Git
-# ------------------------------------------------------------------------------
+DeployGit() {
+    echo -e "${C_GREEN}* git${C_RESET}"
+    cp .gitconfig ~/.gitconfig -v
+}
 
-echo -e "${C_GREEN}* git${C_RESET}"
-cp .gitconfig ~/.gitconfig -v
+DeployTerminator() {
+    echo -e "${C_GREEN}* terminator${C_RESET}"
+    cp -Rv terminator ~/.config
+}
 
+DeployTint2() {
+    echo -e "${C_GREEN}* tint2${C_RESET}"
+    cp -Rv tint2 ~/.config/
+}
 
-# Tint2
-# ------------------------------------------------------------------------------
+DeployVim() {
+    # Copy vim settings
+    echo -e "${C_GREEN}* vim${C_RESET}"
+    cp .vimrc ~/.vimrc -v
 
-echo -e "${C_GREEN}* tint2${C_RESET}"
-cp -Rv tint2 ~/.config/
-
-# Terminator
-# ------------------------------------------------------------------------------
-
-#cp --parents terminator/config ~/.config
-
-# VIM
-# ------------------------------------------------------------------------------
-
-# Copy vim settings
-echo -e "${C_GREEN}* vim${C_RESET}"
-cp .vimrc ~/.vimrc -v
-
-# Download plugin manager (pathogen)
-if [ -f ~/.vim/autoload/pathogen.vim ]; then
-    echo vim pathogen already installed
-else
-    mkdir -p ~/.vim/autoload ~/.vim/bundle
-    wget https://tpo.pe/pathogen.vim -O ~/.vim/autoload/pathogen.vim
-fi
-
-# Vim plugins repositories
-vim_plugins=(
-    https://github.com/bling/vim-airline.git
-    https://github.com/airblade/vim-gitgutter.git
-    https://github.com/tpope/vim-surround.git
-    https://github.com/ciaranm/detectindent.git
-    https://github.com/kien/ctrlp.vim.git
-)
-
-# Install plugins if not present
-cd ~/.vim/bundle
-for url in ${vim_plugins[@]}
-do
-    plugin_name=`basename $url | sed 's/.git$//'`
-    if [ -d $plugin_name ]; then
-        echo "${plugin_name} already installed"
+    # Download plugin manager (pathogen)
+    if [ -f ~/.vim/autoload/pathogen.vim ]; then
+        echo vim pathogen already installed
     else
-        echo -e "${C_YELLOW}installing vim plugin: ${plugin_name}${C_RESET}"
-        git clone --depth=1 $url $plugin_name
+        mkdir -p ~/.vim/autoload ~/.vim/bundle
+        wget https://tpo.pe/pathogen.vim -O ~/.vim/autoload/pathogen.vim
     fi
-done
 
+    # Vim plugins repositories
+    vim_plugins=(
+        https://github.com/bling/vim-airline.git
+        https://github.com/airblade/vim-gitgutter.git
+        https://github.com/tpope/vim-surround.git
+        https://github.com/ciaranm/detectindent.git
+        https://github.com/kien/ctrlp.vim.git
+    )
+
+    # Install plugins if not present
+    cd ~/.vim/bundle
+    for url in ${vim_plugins[@]}
+    do
+        plugin_name=`basename $url | sed 's/.git$//'`
+        if [ -d $plugin_name ]; then
+            echo "${plugin_name} already installed"
+        else
+            echo -e "${C_YELLOW}installing vim plugin: ${plugin_name}${C_RESET}"
+            git clone --depth=1 $url $plugin_name
+        fi
+    done
+}
+
+# Parse args
+# ------------------------------------------------------------------------------
+
+case $1 in
+    'all')
+        DeployBash
+        DeployGit
+        DeployTint2
+        DeployTerminator
+        DeployVim;;
+    'bash')
+        DeployBash;;
+    'git')
+        DeployGit;;
+    'tint2')
+        DeployTint2;;
+    'terminator')
+        DeployTerminator;;
+    'vim')
+        DeployVim;;
+    *)
+        echo "Usage: $0 {bash|git|tint2|terminator|vim}"
+esac
 
